a07889e11997e37840c434f5b9054907394cfb95
Jonghoon Jang <jayjang0209@gmail.com>
2022-01-04 16:18:45 -0800

    Test test branch

--- web_scraper_aws_lambda_source_code.py
+++ web_scraper_aws_lambda_source_code.py
@@ -11,8 +11,8 @@ Serverless Automated Web Scraper - Data ETL (Extract, Transform, Load)
     Database: AWS DynamoDB
               NoSQL database
 
-    created test_jj branch Jan, 2022
-    test
+    created test_a branch Jan, 2022
+    testing
 """
 
 import json
